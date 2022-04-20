90b434339b34e1929d45d21398a2d34a45b51044
ashleyjrobinson <ashjrobinson95@gmail.com>
2022-04-20 08:09:42 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [],
+  purge:[
+  "*.html"
+  ],
+  darkMode: 'class',
+  mode: "jit",
+  theme: {
+    extend: {},
+    fontFamily:{
+      sans: ["Fire Sans", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
